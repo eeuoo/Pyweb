6ff5f966bf5273668ff433876e39f8f19227509d
eeuoo <lee512joo@gmail.com>
2019-02-11 18:06:38 +0900

    2019-02-11

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -1,11 +1,71 @@
 # import sys; 
 # print( sys.path)
 from flask import Flask, g,  Response, make_response, request
-from datetime import datetime, date
-
+from flask import Markup, session, render_template
+from datetime import datetime, date, timedelta
 
 app = Flask(__name__)
 app.debug = True
+app.config.update(
+	SECRET_KEY='X1243yRH!mMwf',
+	SESSION_COOKIE_NAME='pyweb_flask_session',
+	PERMANENT_SESSION_LIFETIME=timedelta(31)      # 31 days
+)
+
+@app.route('/tmpl')
+def t():
+    title = Markup("<strong>title</strong>")
+    mu = Markup("<h1>iii = <i>%s</i></h1>")
+    h = mu % "Italic"
+    print("h=", h)
+
+    return render_template('application.html', Title=title, mu=h)
+
+
+@app.route('/setsession')
+def setSess():
+    session['Token'] = '123X'
+    return "Session이 설정되었습니다!"
+
+@app.route('/getsession')
+def getSess():
+    return session.get('Token')
+
+@app.route('/wc')
+def writeCookie():
+    res = Response("SET COOKIE / SESSION")
+    k = request.args.get('key')
+    v = request.args.get('val')
+    res.set_cookie(k,v)
+
+    return make_response(res)
+
+@app.route('/rc')
+def readCookie():
+    k = request.args.get('key')
+    v = request.cookies.get(k)
+    return "Cookie['" + k + "'] =" + v   
+
+@app.route('/resenv')
+def reseniv():
+    print("is_xhr : ", request.is_xhr)
+    print("endpoint : ",request.endpoint)
+    print("max_content_lenght : ",request.max_content_length)
+
+    return ('REQUEST_METHOD: %(REQUEST_METHOD)s <br>'
+        'SCRIPT_NAME: %(SCRIPT_NAME)s <br>'
+        'PATH_INFO: %(PATH_INFO)s <br>'
+        'QUERY_STRING: %(QUERY_STRING)s <br>'
+        'SERVER_NAME: %(SERVER_NAME)s <br>'
+        'SERVER_PORT: %(SERVER_PORT)s <br>'
+        'SERVER_PROTOCOL: %(SERVER_PROTOCOL)s <br>'
+        'wsgi.version: %(wsgi.version)s <br>'
+        'wsgi.url_scheme: %(wsgi.url_scheme)s <br>'
+        'wsgi.input: %(wsgi.input)s <br>'
+        'wsgi.errors: %(wsgi.errors)s <br>'
+        'wsgi.multithread: %(wsgi.multithread)s <br>'
+        'wsgi.multiprocess: %(wsgi.multiprocess)s <br>'
+        'wsgi.run_once: %(wsgi.run_once)s') % request.environ
 
 
 def ymd(fmt):
@@ -44,10 +104,10 @@ def res1():
 #     print("before_request!!!")
 
 
-@app.route("/gg")
-def helloworld():
-    return "Hello World!" + getattr(g, 'str', '111')
+# @app.route("/gg")
+# def helloworld():
+#     return "Hello World!" + getattr(g, 'str', '111')
 
-@app.route("/")
-def helloworld():
-    return "Hello Flask World!!!!!"
+# @app.route("/")
+# def helloworld():
+#     return "Hello Flask World!!!!!"
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+nodemon --exec /Library/Frameworks/Python.framework/Versions/3.7/bin/python3 start_helloflask.py -w helloflask/__init__.py -w helloflask/templates/application.py
